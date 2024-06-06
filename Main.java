de8b995e3c5e0ffa557cec06019e00d1246ad96f
Henrique Papai <144568237+Henrique0112@users.noreply.github.com>
2024-06-05 21:13:21 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 class Main {
   public static void main(String[] args) {
     //Cadastro pCadastro = new Cadastro();
-    //Login pLogin = new Login();
-    Pedido pPedido = new Pedido();
+    Login pLogin = new Login();
+    //Pedido pPedido = new Pedido();
     //Comanda pComanda = new Comanda();
   }
 }
